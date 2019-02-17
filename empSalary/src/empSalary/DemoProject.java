d73332512a532790c7c28010cd432b4d925f70b3
juttimad <kartai37402@gmail.com>
2019-02-18 03:12:29 +0700

    Add empSalary to ProjectA

--- empSalary/src/empSalary/DemoProject.java
+++ empSalary/src/empSalary/DemoProject.java
@@ -1,10 +1,18 @@
 package empSalary;
 
+import java.util.Scanner;
 public class DemoProject {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args){
+		int id,salary, sales;
+		String name;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter data Employee : ");
+		id = input.nextInt();
+		name = input.next();
+		salary = input.nextInt();
+		sales = input.nextInt();
+		inputProject in = new inputProject(id,name,salary,sales);
+		processProject cess = new processProject(in);
+		outputProject out = new outputProject(in,cess);
 	}
-
 }
--- empSalary/src/empSalary/outputProject.java
+++ empSalary/src/empSalary/outputProject.java
@@ -1,5 +1,10 @@
 package empSalary;
 
 public class outputProject {
-
+	public outputProject(){
+		
+	}
+	public outputProject(inputProject in,processProject cess){
+		System.out.println("Name : "+in.getName()+"\nTotal : "+cess.getSum());
+	}
 }
