0032ced2ce8ae4e308862962e58243bb176293c9
DevFort13 <taaesirisakmaisudjai@gmail.com>
2019-02-18 09:20:17 +0700

    Add directory empSalaryProjectA#

---
+++ empSalaryProjectA#/src/empSalary/inputProject.java
@@ -0,0 +1,43 @@
+package empSalary;
+
+public class inputProject {
+	private int sales;
+	private int id;
+	private String name;
+	private int salary;
+	
+	public inputProject (int id, String n,int salary,int sales){
+		this.id = id;
+		this.name = n;
+		this.salary = salary;
+		this.sales =sales;
+	}
+	
+	public int getId(){
+		return this.id;
+	}
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public int getSalary(){
+		return this.salary;
+	}
+	public void setSalary(int salary){
+		this.salary = salary;
+	}
+	
+	public int getSales(){
+		return this.sales;
+	}
+	public void setSales(int sales){
+		this.sales = sales;
+	}
+}
